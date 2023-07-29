2c268be8486dcb3863f4a88f18c475a6cfe30650
Lisand <boevladik@gmail.com>
2023-07-29 13:41:59 +0300

    add JS DOC

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ imageHandler.runTheFunctionAfterImported(async () => {
     imageGenerator.overlayImage(userImage, selectedPicture, 0, 0);
   });
 
-  parameterСhange.update((position, size) => {
+  parameterСhange.exportPrameters((position, size) => {
     imageGenerator.overlayImage(
       userImage,
       overlayImage,
@@ -26,26 +26,27 @@ imageHandler.runTheFunctionAfterImported(async () => {
     console.log(position, size);
   });
 
-  imageHandler.exportImage();
+  imageHandler.downloadingImage();
 });
 
 /**
- * @typedef {Object} ImageGenerator - объект, содержащий методы для отрисовки изображений на холсте
- * @property {function(url: string): void} addMainImage - метод для добавления главного изображения на холст
- * @property {function(url: string): void} overlayImage - метод для добавления изображения поверх уже нарисованного на холсте
+ * Обработчик отрисовки изображения содержет следующие методы:
+ * @returns {object} addMainImage() - Добавление основного изображения
+ * @returns {object} overlayImage() - Наложение изображения
  */
 function createImageGenerator() {
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
 
   /**
-   * Загружает изображение по указанному URL и отрисовывает его на холсте.
-   * Если isMainImage равен true, то изменяет размеры холста в соответствии с размерами изображения.
+   * Отрисовывает изображение на холсте с задачами параметрами
    * @param {string} url - URL изображения
-   * @param {boolean} typeImage - флаг, указывающий, является ли изображение главным
-   * @returns {void}
+   * @param {string} typeImage - Тип изображения (main или overlay)
+   * @param {number} x - Координата X верхнего левого угла изображения
+   * @param {number} y - Координата Y верхнего левого угла изображения
+   * @param {number} width - Ширена изображения
+   * @param {number} height - Высота изображения
    */
-
   function drawImage(url, typeImage, x, y, width, height) {
     if (!url || !typeImage) {
       throw new Error("Указанны не все параметры");
@@ -68,10 +69,23 @@ function createImageGenerator() {
   }
 
   return {
+    /**
+     * Добавляет основное изображение на холст.
+     * @param {string} url - URL изображения
+     */
     addMainImage(url) {
       drawImage(url, "main");
     },
 
+    /**
+     * Налагает дополнительное изображение на основное.
+     * @param {string} mainPicture - URL основного изображения
+     * @param {string} additionalImage - URL дополнительного изображения
+     * @param {number} x - Координата X верхнего левого угла изображения
+     * @param {number} y - Координата Y верхнего левого угла изображения
+     * @param {number} [width=500] - Ширина изображения
+     * @param {number} [height=500] - Высота изображения
+     */
     overlayImage(mainPicture, additionalImage, x, y, width, height) {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       drawImage(mainPicture, "main");
@@ -80,13 +94,19 @@ function createImageGenerator() {
   };
 }
 
+/**
+ * Обработчик изображения, содержащий методы:
+ * @returns {Object} `importImageFromTheUser()`
+ * @returns {Object} `runTheFunctionAfterImported()`
+ * @returns {Object} `downloadingImage()`
+ */
 function createImageHandler() {
   const inputImportFile = document.querySelector(".result__send-file");
   return {
-    async runTheFunctionAfterImported(newFunction) {
-      inputImportFile.addEventListener("change", newFunction);
-    },
-
+    /**
+     * Получает изображение от пользователя
+     * @returns {void}
+     */
     async importImageFromTheUser() {
       const file = inputImportFile.files[0];
       const reader = new FileReader();
@@ -99,7 +119,19 @@ function createImageHandler() {
       });
     },
 
-    exportImage() {
+    /**
+     * Запускает выполнение функции после того как пользовательское изображение будет загруженно.
+     * @param {function} newFunction - функция которая будет выполнена
+     * @returns {void}
+     */
+    async runTheFunctionAfterImported(newFunction) {
+      inputImportFile.addEventListener("change", newFunction);
+    },
+
+    /**
+     * Запускает скачивание изображения
+     */
+    downloadingImage() {
       const download = document.querySelector("#download-imege");
 
       download.addEventListener("click", () => {
@@ -114,19 +146,31 @@ function createImageHandler() {
   };
 }
 
+/**
+ * Обработчик выбора накладываемого изображения, содержащий методы:
+ * @returns `clickEvents()` - Запускает запускает передаваемую функцию при выборе картинки.
+ */
 function createSelectionImageOverlay() {
   const picturesBox = document.querySelector(".settings__pictures-box");
 
   return {
     clickEvents(newFunction) {
       picturesBox.addEventListener("click", (event) => {
-        if (!event.target.classList.contains("settings__pictures")) return;
+        if (!event.target.classList.contains("settings__pictures")) {
+          return;
+        }
+
         newFunction(event);
       });
     },
   };
 }
 
+/**
+ * Обработчик изменения параметров, содержащий методы:
+ * @param {String} wrapButtons - ID элемента в котором находятся кнопки управления
+ * @returns {Object} `exportPrameters()` - Передаёт изменённые параметры
+ */
 function createParameterСhange(wrapButtons) {
   if (!wrapButtons) {
     throw new Error("Не указан один из параметров");
@@ -137,6 +181,9 @@ function createParameterСhange(wrapButtons) {
   const position = { x: 0, y: 0 };
   const size = { width: 500, height: 500 };
 
+  /**
+   * Изменяет X,Y координаты накладываемого изображения
+   */
   function moveImage() {
     const moveAmount = 100;
 
@@ -173,6 +220,9 @@ function createParameterСhange(wrapButtons) {
   }
   moveImage();
 
+  /**
+   * Изменяет размер накладываемого изображения
+   */
   function resizeImage() {
     wrap.addEventListener("click", (event) => {
       if (event.target.tagName != "BUTTON") return;
@@ -193,7 +243,11 @@ function createParameterСhange(wrapButtons) {
   resizeImage();
 
   return {
-    update(arrowFunction) {
+    /**
+     * При клике вызывает функцию в которую передаёт изменённые параметры.
+     * @param {callback} arrowFunction - Функция в которую передадутся изменённые параметры.
+     */
+    exportPrameters(arrowFunction) {
       wrap.addEventListener("click", (event) => {
         if (event.target.tagName != "BUTTON") return;
         arrowFunction(position, size);
